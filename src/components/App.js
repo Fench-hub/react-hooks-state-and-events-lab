e95396efc67687b89b2d9f2595fcc9bcc1b2b315
felona jepchumba <felonajepchumba@gmail.com>
2025-07-14 12:04:23 +0300

    Implement dark mode toggle, add-to-cart state, and category filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  function handleClick() {
+    setIsDarkMode(!isDarkMode);
+  }
 
   return (
-    <div className={appClass}>
+    <div className={appClass} data-testid="app-container">
       <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <h1>Grocery List</h1>
+        <button onClick={handleClick}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList />
     </div>
   );
 }
